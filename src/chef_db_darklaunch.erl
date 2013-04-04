f39aa499a6e0575615838f93a0c876a34e7a4ab4
Mark Anderson <mark@opscode.com>
2013-04-03 18:34:58 -0700

    Switch to more optimistic matching on #darklaunch field when we're not using dark launch.

--- src/chef_db_darklaunch.erl
+++ src/chef_db_darklaunch.erl
@@ -19,13 +19,15 @@
 
 -export([is_enabled/2]).
 
+-include_lib("eunit/include/eunit.hrl").
+
 %% The darklaunch module used by chef_db can be set using this
 %% define. The default included here ignores `OrgName' and answers
 %% false to all couchdb_* features and true otherwise.
 -ifndef(CHEF_DB_DARKLAUNCH).
-is_enabled(<<"couchdb_", _Rest/binary>>, undefined) ->
+is_enabled(<<"couchdb_", _Rest/binary>>, _) ->
     false;
-is_enabled(_, undefined) ->
+is_enabled(_, _) ->
     true.
 -else.
 is_enabled(Feature, Darklaunch) ->
